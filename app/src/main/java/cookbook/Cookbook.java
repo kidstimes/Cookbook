e70652c3772d045e05c3880257271c497e5db00b
Lian Shi <ls224tk@student.lnu.se>
2023-04-17 19:55:22 +0200

    Replace Cookbook.java

--- app/src/main/java/cookbook/Cookbook.java
+++ app/src/main/java/cookbook/Cookbook.java
@@ -6,11 +6,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 
+//Main class and entry point for the JavaFX application
 public class Cookbook extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-      // Init root controller
+      // Init controller manager to manage the user interface
       new ControllerManager(primaryStage);
     }
 
