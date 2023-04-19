0a2c8e4f1ab693933d9dd5284b08d434962e9290
Lian Shi <ls224tk@student.lnu.se>
2023-04-19 19:06:04 +0200

    Delete ControllerFactory.java

--- app/src/main/java/cookbook/controller/ControllerFactory.java
+++
@@ -1,9 +0,0 @@
-package cookbook.controller;
-
-public interface ControllerFactory {
-  public HomePageController createHomePageController(ControllerManager controllerManager);
-
-  public BrowserController createBrowserController(ControllerManager controllerManager);
-
-  public RecipeController createRecipeController(ControllerManager controllerManager);
-}
